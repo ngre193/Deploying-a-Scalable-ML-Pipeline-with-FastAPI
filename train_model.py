0560bb9029793d44483803a6c43e08e592e90f99
ngre193 <ngre193@wgu.edu>
2026-02-08 14:22:23 -0800

    Update train_model.py
    Declared train dataset as X

--- train_model.py
+++ train_model.py
@@ -41,13 +41,13 @@ X_train, y_train, encoder, lb = process_data(
     # use the train dataset 
     # use training=True
     # do not need to pass encoder and lb as input
-    train,
+    X=train,
     categorical_features=cat_features,
     label="Salary",
     training=True,
 )
 
-X, X_test, y_test, _, _ = process_data(
+X_test, y_test, _, _ = process_data(
     test,
     categorical_features=cat_features,
     label="salary",
