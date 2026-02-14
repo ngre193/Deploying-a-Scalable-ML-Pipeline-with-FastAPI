b74aed3c7a6796738093645f7d244aa5ceff219f
ngre193 <ngre193@wgu.edu>
2026-02-14 10:48:34 -0800

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -16,7 +16,7 @@ from ml.model import (
 project_path = os.getcwd()
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
-data = pd.read_csv("census.csv") # your code here
+data = pd.read_csv(data_path) # your code here
 print(data.head(15))
 
 # TODO: split the provided data to have a train dataset and a test dataset
