1dd3d36ca659f148ed38d9a642077c3f261b7b64
ngre193 <ngre193@wgu.edu>
2026-02-15 13:47:55 -0800

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -21,7 +21,7 @@ print(data)
 
 # TODO: split the provided data to have a train dataset and a test dataset
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.2, random_state=42)  # Your code here
+train, test = train_test_split(data, test_size=0.2, random_state=678)  # Your code here
 
 # DO NOT MODIFY
 cat_features = [
